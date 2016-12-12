6287552d1753e51554334e18802a45ecc5dff46f
Giacomo Zara <giacomo.zara@studenti.unitn.it>
2016-12-12 17:49:54 +0100

    Aggiunto modal contact renter

--- View/JS/modals.js
+++ View/JS/modals.js
@@ -8,18 +8,21 @@ var register_modal = document.getElementById("register_modal");
 var login_modal = document.getElementById("login_modal");
 var add_insertion_modal = document.getElementById("add_insertion_modal");
 var modify_insertion_modal = document.getElementById("modify_insertion_modal");
+var contact_renter_modal = document.getElementById("contact_renter_modal");
 
 // buttons
 var register_btn = document.getElementById("register_button");
 var login_btn = document.getElementById("login_button");
 var add_insertion_btn = document.getElementById("add_insertion_button");
 var modify_insertion_btn = document.getElementById("modify_insertion_button");
+var contact_renter_btn = document.getElementById("contact_renter_button");
 
 // close buttons
 var close_register = document.getElementsByClassName("close_register")[0];
 var close_login = document.getElementsByClassName("close_login")[0];
 var close_add_insertion = document.getElementsByClassName("close_add_insertion")[0];
 var close_modify_insertion = document.getElementsByClassName("close_modify_insertion")[0];
+var close_contact_renter = document.getElementsByClassName("close_contact_renter")[0];
 
 
 // if the register button is clicked, show the modal by changing its visibility value
@@ -32,10 +35,17 @@ login_btn.onclick = function() {
     login_modal.style.display = "block";
 }
 
-// if the add insertion button exists and is clicked, show the modal by changing its visibility value
-if(add_insertion_btn != null){
+// if the add insertion button is clicked, show the modal by changing its visibility value
+if(add_insertion_btn != null){							// checks whether the button is defined (only in user.tpl)
 	add_insertion_btn.onclick = function() {
 		add_insertion_modal.style.display = "block";
+	}
+}
+
+// if the contact-renter button is clicked, show the modal by changing its visibility value
+if(contact_renter_btn != null){							// checks whether the button is defined (only in user.tpl)
+	contact_renter_btn.onclick = function() {
+		contact_renter_modal.style.display = "block";
 	}
 }
 
@@ -43,7 +53,7 @@ if(add_insertion_btn != null){
 this function shows the modify-insertion modal, and fills the "title" field of the form with the title
 of the insertion that is being modified
 */
-if(modify_insertion_btn != null){
+if(modify_insertion_btn != null){						// checks whether the button is defined (only in user.tpl)
 	modify_insertion_btn.onclick = function() {
 		var btn_form = document.getElementById("modify_btn_form");
 		var mdf_form = document.getElementById("modify_form");
@@ -69,11 +79,18 @@ if(add_insertion_btn != null){							// checks whether the button is defined (on
 	}
 }
 
-if(modify_insertion_btn != null){							// checks whether the button is defined (only in user.tpl)
+if(modify_insertion_btn != null){						// checks whether the button is defined (only in user.tpl)
 	close_modify_insertion.onclick = function() {
 		modify_insertion_modal.style.display = "none";
 	}
 }
+
+if(contact_renter_btn != null){							// checks whether the button is defined (only in user.tpl)
+	close_contact_renter.onclick = function() {
+		contact_renter_modal.style.display = "none";
+	}
+}
+
 
 // if the user clicks anywhere outside the modal, close the modal by changing its visibility value
 window.onclick = function(event) {
